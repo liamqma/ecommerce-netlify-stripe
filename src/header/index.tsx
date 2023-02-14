8b9ef7d94ca1b865812331a245d5c6ead9ceb13e
Liam Ma <lma2@atlassian.com>
2023-02-14 22:29:24 +1100

    Add user icon

--- src/header/index.tsx
+++ src/header/index.tsx
@@ -106,9 +106,24 @@ function SectionHeader() {
                         </ul>
                     </nav>
                     <div className="header__icons">
-                        <a href="/cart" className="header__icon header__icon--cart link focus-inset" id="cart-icon-bubble">
-                            <svg className="icon icon-cart" aria-hidden="true" focusable="false" role="presentation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" fill="none">
-                                <path fill="currentColor" fillRule="evenodd" d="M20.5 6.5a4.75 4.75 0 00-4.75 4.75v.56h-3.16l-.77 11.6a5 5 0 004.99 5.34h7.38a5 5 0 004.99-5.33l-.77-11.6h-3.16v-.57A4.75 4.75 0 0020.5 6.5zm3.75 5.31v-.56a3.75 3.75 0 10-7.5 0v.56h7.5zm-7.5 1h7.5v.56a3.75 3.75 0 11-7.5 0v-.56zm-1 0v.56a4.75 4.75 0 109.5 0v-.56h2.22l.71 10.67a4 4 0 01-3.99 4.27h-7.38a4 4 0 01-4-4.27l.72-10.67h2.22z"></path>
+                        <a style={{ marginRight: 0 }} href="https://billing.stripe.com/p/login/test_5kAeWl82y7gL3w45kk" className="header__icon header__icon--cart link focus-inset">
+                            <svg
+                                viewBox="0 0 1024 1024"
+                                fill="currentColor"
+                                height="2em"
+                                width="2em"
+                            >
+                                <path d="M858.5 763.6a374 374 0 00-80.6-119.5 375.63 375.63 0 00-119.5-80.6c-.4-.2-.8-.3-1.2-.5C719.5 518 760 444.7 760 362c0-137-111-248-248-248S264 225 264 362c0 82.7 40.5 156 102.8 201.1-.4.2-.8.3-1.2.5-44.8 18.9-85 46-119.5 80.6a375.63 375.63 0 00-80.6 119.5A371.7 371.7 0 00136 901.8a8 8 0 008 8.2h60c4.4 0 7.9-3.5 8-7.8 2-77.2 33-149.5 87.8-204.3 56.7-56.7 132-87.9 212.2-87.9s155.5 31.2 212.2 87.9C779 752.7 810 825 812 902.2c.1 4.4 3.6 7.8 8 7.8h60a8 8 0 008-8.2c-1-47.8-10.9-94.3-29.5-138.2zM512 534c-45.9 0-89.1-17.9-121.6-50.4S340 407.9 340 362c0-45.9 17.9-89.1 50.4-121.6S466.1 190 512 190s89.1 17.9 121.6 50.4S684 316.1 684 362c0 45.9-17.9 89.1-50.4 121.6S557.9 534 512 534z" />
+                            </svg>
+                        </a>
+                        <a href="/cart" className="header__icon header__icon--cart link focus-inset">
+                            <svg
+                                fill="currentColor"
+                                viewBox="0 0 16 16"
+                                height="1.8em"
+                                width="1.8em"
+                            >
+                                <path d="M8 1a2.5 2.5 0 012.5 2.5V4h-5v-.5A2.5 2.5 0 018 1zm3.5 3v-.5a3.5 3.5 0 10-7 0V4H1v10a2 2 0 002 2h10a2 2 0 002-2V4h-3.5zM2 5h12v9a1 1 0 01-1 1H3a1 1 0 01-1-1V5z" />
                             </svg>
                             <span className="visually-hidden">Cart</span>
                             {items.length > 0 && <div className="cart-count-bubble">
