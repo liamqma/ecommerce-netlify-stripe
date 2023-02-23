718d9fca840e0304033e5d43d82a11a45d609983
Liam Ma <lma2@atlassian.com>
2023-02-23 15:23:15 +1100

    Revert "Site under construction"
    This reverts commit 2505cb3b599fb47560c7de410c8b154aa4ae6dec.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,84 @@
-console.log('Hello!');
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import Cookies from 'js-cookie'
+import './theme.css';
+import './index.css';
+import Home from './pages/home';
+import Products from './pages/products';
+import Product from './pages/product';
+import NotFound from './pages/not-found';
+import Cart from './pages/cart';
+import ThankYou from './pages/thank-you';
+import { useCart } from './hooks/cart';
+import AnnouncementBar from './announcement-bar';
+import Header from './header';
+import Footer from './footer';
+import FullBackgroundImage from './full-background-image';
+import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
-const hello = {}
+const PersistCart = () => {
+  const [{ items }] = useCart();
+  useEffect(() => {
+    Cookies.set('items', JSON.stringify(items));
+  }, [items])
+  return null;
+}
 
-export default hello;
+const Layout = () =>
+  <>
+    <FullBackgroundImage />
+    <AnnouncementBar />
+    <Header />
+    <Outlet />
+    <Footer />
+    <PersistCart />
+  </>
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        element: <Home />,
+        index: true
+      },
+      {
+        path: "/collections/collection-1",
+        element: <Products />,
+      },
+      {
+        path: "/products/:id",
+        element: <Product />,
+        errorElement: <NotFound />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/thank-you",
+        element: <ThankYou />,
+      },
+      {
+        path: "/*",
+        element: <NotFound />
+      }
+    ]
+  },
+]);
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
