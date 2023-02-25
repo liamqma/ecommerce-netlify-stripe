35c8a5a5dea3d798797f8b8a9e9a5847a5c0269f
Liam Ma <lma2@atlassian.com>
2023-02-25 22:52:43 +1100

    Anouncement bar

--- src/announcement-bar/index.tsx
+++ src/announcement-bar/index.tsx
@@ -1,16 +1,26 @@
+import { css } from '@emotion/css'
+
 function SectionAnnouncementBar() {
     return (
-        <div className="announcement-bar color-background-2 gradient" role="region" aria-label="Announcement">
-            <a href="/collections/leia-bryans" className="announcement-bar__link link link--text focus-inset animate-arrow">
-                <p className="announcement-bar__message h5">
-                    A very important announcement
-                    <svg viewBox="0 0 14 10" fill="none" aria-hidden="true" focusable="false" role="presentation" className="icon icon-arrow" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" clipRule="evenodd" d="M8.537.808a.5.5 0 01.817-.162l4 4a.5.5 0 010 .708l-4 4a.5.5 0 11-.708-.708L11.793 5.5H1a.5.5 0 010-1h10.793L8.646 1.354a.5.5 0 01-.109-.546z" fill="currentColor">
-                        </path>
-                    </svg>
-                </p>
-            </a>
-        </div>
+        <a href="/collections/leia-bryans" className={css({
+            backgroundColor: "var(--color-1)",
+            display: 'block',
+            padding: '1rem 2rem',
+            textDecoration: 'none',
+            color: 'white',
+            fontSize: '1.4rem',
+            textAlign: 'center'
+        })}>
+            A very important announcement
+            <svg className={css({
+                marginLeft: '.4rem',
+                verticalAlign: 'middle',
+                width: '1.5rem'
+            })} viewBox="0 0 14 10" fill="none" aria-hidden="true" focusable="false" role="presentation" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" clipRule="evenodd" d="M8.537.808a.5.5 0 01.817-.162l4 4a.5.5 0 010 .708l-4 4a.5.5 0 11-.708-.708L11.793 5.5H1a.5.5 0 010-1h10.793L8.646 1.354a.5.5 0 01-.109-.546z" fill="currentColor">
+                </path>
+            </svg>
+        </a>
     );
 }
 
