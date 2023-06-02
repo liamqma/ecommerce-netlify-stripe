ee473d90cf7a1d7e45108998a2ec0b5e89827cbb
Liam Ma <lma2@atlassian.com>
2023-06-02 18:24:15 +1000

    Test DEPLOY_PRIME_URL

--- netlify/functions/hello-world/hello-world.ts
+++ netlify/functions/hello-world/hello-world.ts
@@ -1,12 +1,12 @@
 import { Handler } from '@netlify/functions';
 
-const URL = process.env.URL;
+const DEPLOY_PRIME_URL = process.env.DEPLOY_PRIME_URL;
 
 export const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, ${URL}!`,
+      message: `Hello, ${DEPLOY_PRIME_URL}!`,
     }),
   };
 };
